5d7f999cf17f0cb7850fda13d89b1e67a9337024
Kyle Chapman <kyleichapman@gmail.com>
2020-10-10 13:29:24 +0200

    Move macros from defines.hpp into ini.hpp

--- src/ini.hpp
+++ src/ini.hpp
@@ -7,7 +7,37 @@
 #include <set>
 #include <string>
 
-#include "defines.hpp"
+#ifndef ALLOW_MULTILINE
+#	define ALLOW_MULTILINE 1
+#endif
+
+#ifndef ALLOW_COMMENTS
+#	define ALLOW_COMMENTS 1
+#endif
+
+#ifndef ALLOW_INLINE_COMMENTS
+#	define ALLOW_INLINE_COMMENTS 1
+#endif
+
+#ifndef STOP_ON_FIRST_ERROR
+#	define STOP_ON_FIRST_ERROR 0
+#endif
+
+#if ALLOW_COMMENTS
+#	ifndef START_COMMENT_PREFIXES
+#		define START_COMMENT_PREFIXES ";#"
+#	endif
+#endif
+
+#if ALLOW_INLINE_COMMENTS
+#	ifndef INLINE_COMMENT_PREFIXES
+#		define INLINE_COMMENT_PREFIXES ";"
+#	endif
+#endif
+
+#define MAX_SECTION_LENGTH 50
+#define MAX_KEY_LENGTH     50
+#define MAX_LINE_LENGTH    200
 
 /**
  * @brief The different types of errors that may occur.
