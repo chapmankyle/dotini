6488131c849ea37dd20084a295703e687e921eb0
Kyle Chapman <kyleichapman@gmail.com>
2020-10-10 13:24:34 +0200

    Test all methods of getting data from .ini file

--- test/main.cpp
+++ test/main.cpp
@@ -13,9 +13,31 @@ int main() {
 		return 1;
 	}
 
+	std::string title = reader.getString("WINDOW", "Title", "");
+
+	double fov = reader.getDouble("GRAPHICS", "FOV", 0.0);
+	bool vsync = reader.getBool("GRAPHICS", "VSYNC", false);
+
+	double masterVol = reader.getDouble("AUDIO", "Master", 0.0);
+	double backgroundVol = reader.getDouble("AUDIO", "Background", 0.0);
+	double subtitleVol = reader.getDouble("AUDIO", "Subtitle", 0.0);
+
+	std::cout << "Window title: " << title << '\n';
 	std::cout << '\n';
+	std::cout << "Field of view: " << fov << '\n';
+	std::cout << "Vsync: " << std::boolalpha << vsync << std::noboolalpha << '\n';
+	std::cout << '\n';
+	std::cout << "Master volume: " << masterVol << '\n';
+	std::cout << "Background volume: " << backgroundVol << '\n';
+	std::cout << "Subtitle volume: " << subtitleVol << '\n';
+
+	std::cout << "\nSections present in file:\n";
 	for (const auto &sec : reader.getSectionNames()) {
 		std::cout << sec << '\n';
+
+		for (const auto &field : reader.getSectionFields(sec)) {
+			std::cout << "    " << field.toString() << '\n';
+		}
 	}
 
 	return 0;
